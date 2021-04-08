426b581d46e662ab64550a7ddea753ed3c882f9e
Kyraas <mariya-bondarenko@mail.ru>
2021-04-08 17:57:48 +0300

    'idfk'

--- Assets/Scripts/playerControll.cs
+++ Assets/Scripts/playerControll.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class playerControll : MonoBehaviour
 {
-	public float speed = 3.0f;
-	public float horizontalInput;
-	public float verticalInput;
+	Animator animator;
+    int runStateHash = Animator.StringToHash("runnung");
+    
+    public float speed = 3.0f;
+    public float horizontalInput;
+    public float verticalInput;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-	horizontalInput = Input.GetAxis("Horizontal");
-	verticalInput = Input.GetAxis("Vertical");
+// Update is called once per frame
+void Update() {
 
-	transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-	transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    horizontalInput = Input.GetAxis("Horizontal");
+    animator.SetFloat("Strafe", horizontalInput);
+
+    verticalInput = Input.GetAxis("Vertical");
+    animator.SetFloat("Forward", verticalInput);
+    
+    AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+    transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }
 }
