317ce58492c0fa095afa32b11c415eb5fae933ad
Kyraas <mariya-bondarenko@mail.ru>
2021-04-09 18:32:28 +0300

    Character looks at mouse, but vector of movement is broken

--- Assets/Scripts/playerControll.cs
+++ Assets/Scripts/playerControll.cs
@@ -7,18 +7,12 @@ public class playerControll : MonoBehaviour
 	Animator animator;
     int runStateHash = Animator.StringToHash("runnung");
     
-    public float speed = 3.0f;
+    public float rspeed = 3.0f;
+    public float speed = 1.0f;
     public float horizontalInput;
     public float verticalInput;
 
-    // Start is called before the first frame update
-    void Start() {
-        animator = GetComponent<Animator>();
-    }
-
-// Update is called once per frame
-void Update() {
-
+    void Movement(){
     horizontalInput = Input.GetAxis("Horizontal");
     animator.SetFloat("Strafe", horizontalInput);
 
@@ -29,5 +23,27 @@ void Update() {
 
     transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
     transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+}
+    void CamFollow(){
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast (ray, out hitdist)) {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rspeed * Time.deltaTime);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+// Update is called once per frame
+void Update() {
+    Movement();
+    CamFollow();
     }
 }
+
